c45467f882acd6366f0012e8b556e516b156b5c6
Asish Samantaray <samantaray.asish04@gmail.com>
2020-08-31 23:20:31 +0530

    Second Commit

--- src/main/java/com/asish/restservice/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/asish/restservice/SpringbootBuildingblocksApplication.java
@@ -3,6 +3,7 @@ package com.asish.restservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
